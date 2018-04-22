9af9ae483bcf64427a04cb49e788d4fb0927ffea
Rory Dungan <rorydungan@gmail.com>
2018-04-22 13:15:11 +1000

    Separate photo and viewfinder cameras

---
+++ Assets/Code/PhotoCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PhotoCamera : MonoBehaviour
+{
+    ////////////////////////////////////////
+    // Cached references to other objects
+    ////////////////////////////////////////
+    [SerializeField]
+    private GameObject viewfinderCamera;
+
+    [SerializeField]
+    private Camera photoCamera;
+
+    private GameObject mainCamera;
+
+    ////////////////////////////////////////
+    // State
+    ////////////////////////////////////////
+    private bool photoMode;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(viewfinderCamera, "Viewfinder Camera not assigned to PhotoCamera");
+        Assert.IsNotNull(photoCamera, "Photo Camera not assigned to PhotoCamera");
+
+        mainCamera = Camera.main.gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            TogglePhotoMode();
+        }
+    }
+
+    private void TogglePhotoMode()
+    {
+        photoMode = !photoMode;
+
+        mainCamera.SetActive(!photoMode);
+        viewfinderCamera.SetActive(photoMode);
+    }
+}
