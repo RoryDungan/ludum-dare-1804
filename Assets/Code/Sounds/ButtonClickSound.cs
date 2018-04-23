ef05effeba157ff1a0220cd58f9cd6aee6367e2c
Rory Dungan <rorydungan@gmail.com>
2018-04-23 19:46:09 +1000

    Add click sound to button

---
+++ Assets/Code/Sounds/ButtonClickSound.cs
@@ -0,0 +1,28 @@
+﻿using FMODUnity;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonClickSound : MonoBehaviour
+{
+    private const string clickSound = "event:/UI Button";
+
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(ButtonClicked);
+    }
+
+    private void OnDestroy()
+    {
+        button.onClick.RemoveListener(ButtonClicked);
+    }
+
+    private void ButtonClicked()
+    {
+        RuntimeManager.PlayOneShot(clickSound);
+    }
+}
